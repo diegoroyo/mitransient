9455460d8af7f53b31ee6628051db733c68f9ded
Miguel Crespo <mcrescas@gmail.com>
2024-03-14 18:03:10 +0100

    NLOS capture meter simplified + support for spectral

--- mitransient/sensors/nloscapturemeter.py
+++ mitransient/sensors/nloscapturemeter.py
@@ -3,19 +3,15 @@ from __future__ import annotations  # Delayed parsing of type annotations
 import drjit as dr
 import mitsuba as mi
 
-from mitsuba import Log, LogLevel
-
-# NOTE(diego): idk why, but mitsuba's variant is not set for NLOSCaptureMeter.__init__ method
-# it is fine if we import it here
-from mitsuba import Emitter, Float, Transform4f
-
-from mitsuba import ScalarVector2u
+from mitsuba import Log, LogLevel, is_spectral, Transform4f, ScalarVector2u
 from drjit.scalar import Array3f as ScalarArray3f  # type: ignore
 
 from typing import Tuple
 
+from .nlossensor import NLOSSensor
+from ..utils import indent
 
-class NLOSCaptureMeter(mi.Sensor):
+class NLOSCaptureMeter(NLOSSensor):
     """
         `nlos_capture_meter` plugin
         ===========================
@@ -68,28 +64,6 @@ class NLOSCaptureMeter(mi.Sensor):
     def __init__(self, props: mi.Properties):
         super().__init__(props)
 
-        # try to get the emitter associated with the NLOSCaptureMeter
-        self.emitter = None
-        for name in props.property_names():
-            if not name.startswith('_arg_'):
-                continue
-
-            value = props.get(name)
-            if value is None:
-                continue
-            if not isinstance(value, Emitter):
-                continue
-
-            if self.emitter is not None:
-                raise Exception(
-                    'Only one emitter can be specified per NLOS capture meter.')
-
-            self.emitter = value
-
-        if self.emitter is None:
-            raise Exception(
-                'No laser emitter was specified for the NLOS capture meter.')
-
         self.needs_sample_3: bool = False
 
         self.account_first_and_last_bounces: bool = \
@@ -99,40 +73,31 @@ class NLOSCaptureMeter(mi.Sensor):
             Transform4f.translate(
                 props.get('sensor_origin', ScalarArray3f(0)))
 
-        self.is_confocal: bool = props.get('confocal', False)
-        self.film_size: ScalarVector2u = self.film().size()
-        if self.is_confocal:
-            # NOTE: uses custom python interface for c++'s set_size method
-            self.film().set_size(ScalarVector2u(1, 1))
+        # Distance between the laser origin and the focusing point
+        # Should be provided by the user if needed
+        self.laser_bounce_opl = mi.Float(0)
 
-        self.laser_origin: ScalarArray3f = props.get(
-            'laser_origin', ScalarArray3f(0))
-        laser_lookat3_pixel = props.get(
-            'laser_lookat_pixel', ScalarArray3f(-1))
-        laser_lookat3_3d = props.get('laser_lookat_3d', ScalarArray3f(0))
-        self.laser_lookat_is_pixel: bool = \
-            laser_lookat3_pixel.x >= 0.0 and laser_lookat3_pixel.y >= 0.0
-        if self.laser_lookat_is_pixel:
-            film_width, film_height = self.film_size
-            if laser_lookat3_pixel.x < 0.0 or film_width < laser_lookat3_pixel.x:
-                Log(LogLevel.Warn, 'Laser lookat pixel (X position) is out of bounds')
-            if laser_lookat3_pixel.y < 0.0 or film_height < laser_lookat3_pixel.y:
-                Log(LogLevel.Warn, 'Laser lookat pixel (Y position) is out of bounds')
-            if dr.abs(laser_lookat3_pixel.z) > dr.epsilon(Float):
-                Log(LogLevel.Warn, 'Laser lookat pixel (Z position) should be zero')
-            self.laser_lookat: ScalarArray3f = laser_lookat3_pixel
-        else:
-            self.laser_lookat: ScalarArray3f = laser_lookat3_3d
-        self.laser_target = None
+        # Confocal setup pixel if desired by the user (default -1, no confocal)
+        self.confocal_pixel = props.get('confocal_pixel', mi.ScalarPoint2f(-1, -1))
+        self.is_confocal: bool = self.confocal_pixel.x >= 0 and self.confocal_pixel.y >= 0
+
+        film_size: ScalarVector2u = self.film().size()
+        if self.is_confocal and film_size.x != 1 and film_size.y != 1:
+            Log(LogLevel.Error,
+                    f"Confocal configuration requires a film with size [1,1] instead of {film_size}")
+
+        dr.make_opaque(self.laser_bounce_opl, self.confocal_pixel)
 
     def _sensor_origin(self) -> ScalarArray3f:
         return self.world_transform.translation()
 
     def _pixel_to_sample(self, pixel: mi.Point2f) -> mi.Point2f:
-        film_width, film_height = self.film_size
-        return mi.Point2f(
-            pixel.x / film_width,
-            pixel.y / film_height)
+        # film_width, film_height = self.film_size
+        # return mi.Point2f(
+        #     pixel.x / film_width,
+        #     pixel.y / film_height)
+        film_size = self.film().size()
+        return pixel / mi.Point2f(film_size)
 
     def _sample_direction(self,
                           time: mi.Float,
@@ -141,16 +106,17 @@ class NLOSCaptureMeter(mi.Sensor):
         origin = self._sensor_origin()
 
         if self.is_confocal:
-            target = self.laser_target
+            # Confocal sample always center of the pixel
+            grid_sample = self._pixel_to_sample(self.confocal_pixel + 0.5)
+            target = self.shape().sample_position(
+                time, grid_sample, active
+            ).p
         else:
-            film_width, film_height = self.film_size
+            film_size = self.film().size()
             # instead of continuous samples over the whole shape,
             # discretize samples so they only land on the center of the film's
             # "pixels"
-            grid_sample = self._pixel_to_sample(
-                mi.Point2f(
-                    dr.floor(sample.x * film_width) + 0.5,
-                    dr.floor(sample.y * film_height) + 0.5))
+            grid_sample = self._pixel_to_sample(dr.floor(sample * film_size) + 0.5)
             target = self.shape().sample_position(
                 time, grid_sample, active
             ).p  # sampled position of PositionSample3f
@@ -160,64 +126,21 @@ class NLOSCaptureMeter(mi.Sensor):
         direction /= distance
         return distance, direction
 
-    def set_scene(self, scene: mi.Scene):
-        # NOTE uses custom add_emitter function
-        self.emitter.set_scene(scene)
-        scene.add_emitter(self.emitter)
-        scene_emitters = scene.emitters()
-        if len(scene_emitters) > 1:
-            Log(LogLevel.Warn,
-                f'You have defined multiple ({len(scene_emitters)}) emitters in the scene with a NLOS capture meter.')
-
-    def set_shape(self, shape: mi.Shape):
-        # NOTE: set_shape is called by the integrator (see common.py)
-        # super().set_shape(shape)  # sets self.shape
-
-        if self.laser_lookat_is_pixel:
-            self.laser_target = ScalarArray3f(dr.ravel(
-                self.shape().sample_position(
-                    0.0,
-                    self._pixel_to_sample(
-                        mi.Point2f(
-                            self.laser_lookat.x,
-                            self.laser_lookat.y
-                        ))).p))  # sampled position of PositionSample3f
-            Log(LogLevel.Info,
-                'Laser is pointed to pixel ({sx:.5f}, {sy:.5f}), '
-                'which equals to 3D point ({px:.5f}, {py:.5f}, {pz:.5f})'.format(
-                    sx=self.laser_lookat.x, sy=self.laser_lookat.y,
-                    px=self.laser_target.x, py=self.laser_target.y, pz=self.laser_target.z))
-        else:
-            self.laser_target = self.laser_lookat
-            Log(LogLevel.Info,
-                'Laser is pointed to 3D point ({px:.5f}, {py:.5f}, {pz:.5f})'.format(
-                    px=self.laser_target.x, py=self.laser_target.y, pz=self.laser_target.z))
-
-        # NOTE uses custom set_world_transform function
-        lookat = mi.Transform4f.look_at(
-            origin=self.laser_origin,
-            target=self.laser_target,
-            up=mi.Vector3f(0, 1, 0))
-        self.emitter.set_world_transform(lookat)
-
-        self.laser_bounce_opl = dr.norm(
-            self.laser_target - self.laser_origin) * self.IOR_BASE
-
     def sample_ray_differential(
             self, time: mi.Float,
             sample1: mi.Float, sample2: mi.Point2f, sample3: mi.Point2f,
             active: mi.Bool = True) -> Tuple[mi.RayDifferential3f, mi.Color3f]:
+
         origin = self._sensor_origin()
         sensor_distance, direction = self._sample_direction(
             time, sample2, active)
 
-        # TODO call to sample_wavelengths yields
-        # *** RuntimeError: Tried to call pure virtual function "Sensor::sample_wavelengths"
-        # but it is only used in spectral mode (https://github.com/mitsuba-renderer/mitsuba3/blob/ff9cf94323703885068779b15be36345a2eadb89/include/mitsuba/core/spectrum.h#L471)
-        # we do not have support for spectral rendering anyway, transient block is stuck with 3 channels I think
-        wavelengths, wav_weight = [], 1
-        # wavelengths, wav_weight = self.sample_wavelengths(
-        #     dr.zeros(mi.SurfaceInteraction3f), sample1, active)
+        if is_spectral:
+            wav_sample = mi.sample_shifted(sample1)
+            wavelengths, wav_weight = mi.sample_rgb_spectrum(wav_sample)
+        else:
+            wavelengths = []
+            wav_weight = 1.0
 
         if not self.account_first_and_last_bounces:
             time -= self.laser_bounce_opl + sensor_distance * self.IOR_BASE
@@ -246,37 +169,28 @@ class NLOSCaptureMeter(mi.Sensor):
     def traverse(self, callback: mi.TraversalCallback):
         # NOTE: all the parameters are set as NonDifferentiable by default
         super().traverse(callback)
-        callback.put_object("emitter", self.emitter,
-                            mi.ParamFlags.NonDifferentiable)
         callback.put_parameter(
             "needs_sample_3", self.needs_sample_3, mi.ParamFlags.NonDifferentiable)
         callback.put_parameter("account_first_and_last_bounces",
                                self.account_first_and_last_bounces, mi.ParamFlags.NonDifferentiable)
         callback.put_parameter(
             "is_confocal", self.is_confocal, mi.ParamFlags.NonDifferentiable)
-        callback.put_parameter("film_size", self.film_size,
-                               mi.ParamFlags.NonDifferentiable)
-        callback.put_parameter(
-            "laser_origin", self.laser_origin, mi.ParamFlags.NonDifferentiable)
-        callback.put_parameter(
-            "laser_lookat", self.laser_lookat, mi.ParamFlags.NonDifferentiable)
-        callback.put_parameter(
-            "laser_target", self.laser_target, mi.ParamFlags.NonDifferentiable)
         callback.put_parameter(
             "laser_bounce_opl", self.laser_bounce_opl, mi.ParamFlags.NonDifferentiable)
+        callback.put_paramter(
+            "confocal_pixel", self.confocal_pixel, mi.ParamFlags.NonDifferentiable)
 
     def parameters_changed(self, keys):
         super().parameters_changed(keys)
 
     def to_string(self):
         string = f"{type(self).__name__}[\n"
-        string += f"  account_first_and_last_bounces = {self.account_first_and_last_bounces},"
-        string += f"  is_confocal = {self.is_confocal},"
-        string += f"  film_size = {self.film_size},"
-        string += f"  laser_origin = {self.laser_origin},"
-        string += f"  laser_target = {self.laser_target},"
-        string += f"  emitter = {self.emitter},"
-        string += f"  film = {self.film()},"
+        string += f"  laser_bounce_opl = {self.laser_bounce_opl}, \n"
+        string += f"  account_first_and_last_bounces = {self.account_first_and_last_bounces}, \n"
+        string += f"  is_confocal = {self.is_confocal}, \n"
+        if self.is_confocal:
+            string += f"  confocal_pixel = {self.confocal_pixel}, \n"
+        string += f"  film = { indent(self.film()) }, \n"
         string += f"]"
         return string
 
