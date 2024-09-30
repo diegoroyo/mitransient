89ba5f79f296ff645bdffd546e3f8a4185472248
jgarciapueyo <jgarciapueyo@gmail.com>
2024-09-30 14:39:29 +0200

    add: rainbow visualization code

--- mitransient/visualization.py
+++ mitransient/visualization.py
@@ -83,3 +83,46 @@ def show_video(input_sample, axis_video, uint8_srgb=True):
     ani = animation.FuncAnimation(fig, update, frames=num_frames, repeat=False)
     display(HTML(ani.to_html5_video()))
     plt.close()
+
+
+def rainbow_visualization(steady_state, data_transient, modulo, min_modulo, max_modulo, max_time_bins=None, mode="peak_time_fusion", scale_full_fusion=1):
+    import matplotlib.cm as cm
+    time_bins = data_transient.shape[2] if max_time_bins is None else max_time_bins
+
+    # From: http://giga.cps.unizar.es/~ajarabo/pubs/MT/downloads/Jarabo2012_MasterThesis_noannex.pdf
+    # 1. Obtain full fusion (a.k.a steady rendering)
+    full_fusion = np.sum(data_transient, axis=2)
+
+    # 2. Obtain idx of time bin with peak radiance for every pixel i,j
+    # 2.1 Peak radiance according to the max value of RGB for every pixel and time_bin
+    data_transient_max = np.max(data_transient, axis=-1)
+    # 2.2 Compute index
+    idx_data_transient_max = np.argmax(data_transient_max, axis=-1)
+
+    # 3. Create visualization
+    visualization = np.zeros(data_transient.shape)
+    for i in range(visualization.shape[0]):
+        for j in range(visualization.shape[1]):
+            idx = idx_data_transient_max[i, j]
+
+            # Sparse fusion
+            if mode == "sparse_fusion":
+                if min_modulo <= idx % modulo <= max_modulo:
+                    visualization[i,j,idx,:] = data_transient[i,j,idx,:]**scale_full_fusion
+                else:
+                    visualization[i,j,idx,:] = 0
+            elif mode == "rainbow_fusion":
+                if min_modulo <= idx % modulo <= max_modulo:
+                    visualization[i,j,idx,:] = cm.jet(idx/time_bins)[:3]
+                else:
+                    visualization[i,j,idx,:] = 0
+            elif mode == "peak_time_fusion":
+                if min_modulo <= idx % modulo <= max_modulo:
+                    visualization[i,j,idx,:] = cm.jet(idx/time_bins)[:3]
+                else:
+                    visualization[i,j,idx,:] = steady_state[i,j,:]
+            else:
+                raise NotImplementedError("Not implemented")
+    
+    visualization = np.sum(visualization, axis=2)
+    return visualization
