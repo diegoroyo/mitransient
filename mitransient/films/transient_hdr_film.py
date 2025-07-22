a019d8e60f8bb1130ea1fdc0b007eec2dc501a80
Oscar Pueyo-Ciutad <o.pueyo@unizar.es>
2025-07-22 16:23:44 +0200

    After re-running, it seems like scale makes sense

--- mitransient/films/transient_hdr_film.py
+++ mitransient/films/transient_hdr_film.py
@@ -163,6 +163,7 @@ class TransientHDRFilm(mi.Film):
 
         weight = dr.gather(Float, data.array, weight_idx)
         values_ = dr.gather(Float, data.array, values_idx)
+        # print('Shape weight and values', dr.shape(weight), dr.shape(values_))
 
         values = values_ / dr.select((weight == 0.0), 1.0, weight)
 
